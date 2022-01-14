fb56b70db1741b26ffbccbaa4fa9e171acdee295
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-13 21:35:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container} from "react-bootstrap/dist/react-bootstrap.min"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <Container>
+        <h1>Hola mundo</h1>
+      </Container>
     </div>
   );
 }

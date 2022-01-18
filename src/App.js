f9f48aec0292949bbe0eafa0be63eeff3e3660c6
Facundo Colavini <54385792+facundocolavini@users.noreply.github.com>
2022-01-18 18:19:48 -0300

    adding style on my navbar component (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from "react-bootstrap/dist/react-bootstrap.min"
+import TopBar from './components/TopBar/TopBar'
 import './app.scss'
 
 function App() {
   return (
     <div className="App">
-      {/* NAVBAR */}
+      <TopBar/>
       <Container>
         <h1>Hola mundo</h1>
       </Container>

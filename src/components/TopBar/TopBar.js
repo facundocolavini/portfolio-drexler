055312cf99daf8f11288fbee4e65b5d2058dcb69
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-18 18:21:53 -0300

    adding style on my navbar component

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -3,26 +3,22 @@ import {Container,Navbar,Nav} from "react-bootstrap/dist/react-bootstrap.min"
 const TopBar = () => {
     return (
  
-                        
-
-                        <Navbar collapseOnSelect expand="lg" className='navbar' bg="light"  variant="dark">
-                        <Container >
-                            <Navbar.Brand>
-                          
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav className="me-auto"></Nav>
-                                <Nav className="my-2 my-lg-2">
-                                    <Nav.Link className="text-right text-dark" href="#action1">Sobre mí</Nav.Link>
-                                    <Nav.Link className="text-right text-dark" href="#action2">Proyectos</Nav.Link>
-                                    <Nav.Link className="text-right text-dark" href="#action3">Herramientas</Nav.Link>
-                                    <Nav.Link className="text-right text-dark bg-primary" href="#action4"  bg="primary">Contacto</Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
+                <Navbar collapseOnSelect expand="lg" className='navbar' bg="light"  variant="dark">
+                    <Container >
+                        <Navbar.Brand>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto"></Nav>
+                            <Nav className="my-2 my-lg-2">
+                                <Nav.Link className="text-right text-dark" href="#action1">Sobre mí</Nav.Link>
+                                <Nav.Link className="text-right text-dark" href="#action2">Proyectos</Nav.Link>
+                                <Nav.Link className="text-right text-dark" href="#action3">Herramientas</Nav.Link>
+                                <Nav.Link className="text-right text-dark bg-primary" href="#action4"  bg="primary">Contacto</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Container> 
-                        </Navbar>
- 
+                </Navbar>
     )
 }
 

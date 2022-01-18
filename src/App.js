29e8d27825f7338e25b5c68ad23c9ba1f5895018
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-18 15:26:05 -0300

    creating globals files and app.scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from "react-bootstrap/dist/react-bootstrap.min"
+import './app.scss'
 
 function App() {
   return (
     <div className="App">
+      {/* NAVBAR */}
       <Container>
         <h1>Hola mundo</h1>
       </Container>

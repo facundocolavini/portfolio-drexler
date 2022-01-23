12cb1c25d974b97350a9ab5010400a12e4fe99c3
andi25 <adallapozza@hotmail.com>
2022-01-23 15:40:24 -0300

    Intro

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,21 @@
+import zyro from'../../assets/illustrations/zyro-image_1.png';
+import './Intro.scss'
+
+import React from 'react'
+
+function Intro() {
+    return (
+        <div className='Intro d-flex justify-content-center'>
+            <div className='SubIntro container'>
+                <div className='Description'>
+                    <h3 className='title'>Hola, Soy Drexler, <span className='back'>Desarrollador web</span></h3>
+                    <span className='text'>Joven emprendedor con ganas de trabajar y aprender</span>
+                    <button className='btn'>¿HABLAMOS?</button>
+                </div>
+                <img src={zyro} alt="zyro"/>
+            </div>
+        </div>
+    )
+}
+
+export default Intro

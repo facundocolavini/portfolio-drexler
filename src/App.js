84685607b8e315a63445610dc55109948a618b89
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-27 13:38:42 -0300

    fixing styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Container} from "react-bootstrap/dist/react-bootstrap.min"
+/* import {Container} from "react-bootstrap/dist/react-bootstrap.min" */
 import TopBar from './components/TopBar/TopBar'
 import Intro from './components/Intro/Intro'
+import About from './components/About/About'
 import './app.scss'
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
-      <Intro />
-      
+      <Intro/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div id="about">
+        <h1>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptate, quisquam.
+        </p>
+    </div>
+
+  );
+};
+
+export default About;

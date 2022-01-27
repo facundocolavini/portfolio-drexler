c6ee730578b32a3f61b04a939aac57f8c9e668e1
Facundo Colavini <54385792+facundocolavini@users.noreply.github.com>
2022-01-27 15:41:39 -0300

    adding style on section about (#5)

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {Container, Row, Col} from "react-bootstrap/dist/react-bootstrap.min";
+
+import './about.scss';
 
 const About = () => {
   return (
-    <div id="about">
-        <h1>About</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptate, quisquam.
-        </p>
-    </div>
-
+    <Container  id="about">
+        <Row className=" about">
+            <Col xs={12} md={6}>
+                <div className="box-content-1">
+                    <h2 className="about-title ">Sobre</h2>
+                    <h2 className="about-title about-title--variant">Drexler</h2>
+                </div>
+            </Col>
+            <Col xs={12} md={6}>
+                <div className='box-content-2'>
+                    Eficiente Desarrollador Web y Analista en sistemas, dispuesto a ayudar a los demás a solucionar sus intereses de computación, cooperativo en su trabajo e inteligente.
+                </div>
+            </Col>
+        </Row>
+    </Container>
   );
 };
 

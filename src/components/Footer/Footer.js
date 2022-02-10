31b27017adf9291ade73ce014479f077b79ab444
Facundo Colavini <facu_colavini@hotmail.com>
2022-02-10 15:12:24 -0300

    fixing deploy error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Row, Col, Link} from "react-bootstrap/dist/react-bootstrap.min";
+import { Container, Row, Col } from "react-bootstrap/dist/react-bootstrap.min";
 import './footer.scss';
 
 const Footer = () => {
